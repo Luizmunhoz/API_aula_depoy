eb771d733a87ebdc8dea3679382c0f80394ab6c3
Luizmunhoz <munhozluth@gmail.com>
2023-03-10 17:52:34 -0300

    commit inicial

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+    return res.json('API está ok')
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Servidor OK ${port}`)
+})
